a784926d3e26101dd01afec4b1e4126522e1e809
your <vutran6853@gmail.com>
2020-01-05 11:53:31 -0600

    remove unuse code in sidebar, move API call to backend API

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -4,49 +4,24 @@ import { withRouter } from 'react-router'
 import { connect } from 'react-redux'
 import { setUserToStateAction } from '../../redux/rootReducer'
 import { Link } from 'react-router-dom'
-function Sidebar(props) {
-//   let regionList = ['---', 'AU', 'BR', 'CA', 'CH', 'DE', 'DK', 'ES', 'FI', 'FR', 'GB', 'IE', 'IR', 'NO', 'NL', 'NZ', 'TR', 'US']
-//   let renderRegionListTemplate = []
-// console.log({props})
-//   for (let i = 0; i < regionList.length; i++) {
-//     renderRegionListTemplate.push(
-//       <option key={regionList[i]} value={regionList[i]}>{regionList[i]}</option>
-//     )
-//   }
 
-//   function handleFetchUserByRegion(payload) {
-//     console.log('fire')
-//     if (payload !== '---') {
-//       fetch(`https://randomuser.me/api/?nat=${payload}&results=5`)
-//       .then(function (response) {
-//         return response.json()
-//       })
-//       .then(function (response) {
-//         console.log({response})
-//         props.setUserToStateAction(response.results)
-//       })
-//       .catch(function (err) {
-//         console.error('Danger there is error' + err)
-//       })
-//     }
-//     return null
-//   }
+function Sidebar(props) {
   return (
     <aside className="sidebar_container">
       <ul className="sidebar_items">
-        <Link to="">
+        <Link to="/">
           Home
         </Link>
-        <Link to="">
+        <Link to="/message">
           Message
         </Link>
-        <Link to="">
+        <Link to="/user">
           User
         </Link>
-        <Link to="">
+        <Link to="/report">
           Report
         </Link>
-        <Link to="">
+        <Link to="/setting">
           Setting
         </Link>
       </ul>
